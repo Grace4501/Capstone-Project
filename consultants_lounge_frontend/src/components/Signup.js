3b5787857b09a693acf04b1cdbf33ae45761f7c1
benyaminsoldier <daniel.benjumea0@gmail.com>
2025-04-08 09:29:34 -0600

    admin dashboard has issues with routers app

--- consultants_lounge_frontend/src/components/Signup.js
+++ consultants_lounge_frontend/src/components/Signup.js
@@ -105,24 +105,6 @@ export default function Signup() {
             </div>
             <div className="form-container">
 
-                <form>
-                    <h4> Create Your Account </h4>
-                    <div>
-                        <label> Name </label>
-                        <input type="text" />
-                    </div>
-                    <div >
-                        <label>Email:</label>
-                        <input type="email" />
-                    </div>
-                    <div>
-                        <label>Password:</label>
-                        <input type="password" />
-                    </div>
-                    <div className="buttons">
-                        <button className="login-button"> LOGIN </button>
-                        <button className="google"> <img src={googleLogo} alt='GoogleLogo'></img>SIGN UP WITH GOOGLE </button>
-
                 <form onSubmit={handleSubmit}>
                     <h4> Create Your Account </h4>
 
