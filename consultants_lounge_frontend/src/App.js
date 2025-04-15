1275d4fb9adc5ea99c2f5fc3e7d6987aaab275ad
2023317MaxDyson <maxdyson@gmail.com>
2025-04-14 20:12:17 -0600

    Finished the Business Owner Dashboard

--- consultants_lounge_frontend/src/App.js
+++ consultants_lounge_frontend/src/App.js
@@ -1,6 +1,3 @@
-
-
-import Admin from './components/AdminDashboard/Admin.js';
 import './App.css';
 import React from 'react';
 import{Routes, Route, Link} from 'react-router-dom';
@@ -10,22 +7,23 @@ import HomePage from './components/HomePage';
 import SurveyConsultant from './components/survey-Consultant';
 import SurveyBusinesses from './components/survey-Businesses';
 import Login from './components/Login';
-
 import Business from './components/BusinessOwnerDashboard/Business.js';
+import Admin from './components/AdminDashboard/Admin.js';
 
 function App() {
 
   return (
     <div>
-      <Business/>
-     {/* <Routes>
+       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/aboutUs' element={<AboutUs />} />
         <Route path='/signUp' element={<SignUp />} />
         <Route path='/survey-Consultant' element={<SurveyConsultant />} />
         <Route path='/survey-Businesses' element={<SurveyBusinesses />} />
         <Route path='/Login' element={<Login />} />
-      </Routes>  */}
+        <Route path='/Business' element={<Business />} />
+        <Route path='/Admin' element={<Admin />} />
+      </Routes>  
     </div>  
   );
 }
